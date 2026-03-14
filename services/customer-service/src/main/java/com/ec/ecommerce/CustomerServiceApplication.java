69b7b5411c7272ed9b91aa5d08026d0b5356d957
sidiya-dev <sidiya@email.com>
2026-03-14 14:33:01 +0000

    containerized customer service

--- services/customer-service/src/main/java/com/ec/ecommerce/CustomerServiceApplication.java
+++ services/customer-service/src/main/java/com/ec/ecommerce/CustomerServiceApplication.java
@@ -2,6 +2,7 @@ package com.ec.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 public class CustomerServiceApplication {
